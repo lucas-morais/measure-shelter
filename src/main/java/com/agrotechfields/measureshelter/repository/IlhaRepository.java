78b090a580a0a7df1f86532999ea45da2ea4c743
DavioLopes <davioplp@gmail.com>
2022-07-13 19:17:14 -0300

    refactor: mudança de nome ilha repositorio

--- src/main/java/com/agrotechfields/measureshelter/repository/IlhaRepository.java
+++ src/main/java/com/agrotechfields/measureshelter/repository/IlhaRepository.java
@@ -1,9 +1,10 @@
 package com.agrotechfields.measureshelter.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.agrotechfields.measureshelter.model.Ilha;
-
-public interface ilhaRepository  extends MongoRepository<Ilha, String> {
+@Repository
+public interface IlhaRepository  extends MongoRepository<Ilha, String> {
   
 }
