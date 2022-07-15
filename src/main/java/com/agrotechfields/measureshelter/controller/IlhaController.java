0ed1fbf4d0919bbaf685250dd32009e961b9d9c8
rodolfo <rodolfo_mno@hotmail.com>
2022-07-15 01:33:42 -0300

    refactor: change post to put

--- src/main/java/com/agrotechfields/measureshelter/controller/IlhaController.java
+++ src/main/java/com/agrotechfields/measureshelter/controller/IlhaController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -54,7 +55,7 @@ public class IlhaController {
     ilhaService.deletar(id);
   }
 
-  @PostMapping("/{id}")
+  @PutMapping("/{id}")
   public ResponseEntity<IlhaDto> atualizar(@RequestBody Ilha ilha, @PathVariable String id) {
     IlhaDto ilhaAtualizada = ilhaService.Atualizar(ilha, id);
 
