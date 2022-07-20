a7fa25fa218346b3513720b28e26f4aa7b8d9b98
Lucas Toledo <lucasmoraisss@gmail.com>
2022-07-20 20:45:47 -0300

    feat: excecao para recurso nao encontrado

---
+++ src/main/java/com/agrotechfields/measureshelter/error/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.agrotechfields.measureshelter.error;
+
+public class NotFoundException extends RuntimeException {
+
+  public NotFoundException(String message) {
+    super(message);
+  }
+  
+}
