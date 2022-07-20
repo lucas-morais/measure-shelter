212866078f96bf1426442d24e3107fc50d2ba2bb
Lucas Toledo <lucasmoraisss@gmail.com>
2022-07-20 20:49:07 -0300

    feat: excecao para requisicao nao compreendida

---
+++ src/main/java/com/agrotechfields/measureshelter/error/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.agrotechfields.measureshelter.error;
+
+public class BadRequestException extends RuntimeException{
+
+  public BadRequestException(String message) {
+    super(message);
+  }
+}
